e60ef2917adeee551a0557fcff8f669aec66bc88
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-20 04:20:52 -0600

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -6,16 +6,17 @@
 #include <QMainWindow>
 #include <QTreeWidgetItem>
 #include <QFileSystemModel>
-
+#include"zip_explorer.h"
+#include "dialog.h"
 namespace Ui {
 class MainWindow;
 }
 
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-void setModel(QAbstractItemModel *model);
-void createTree();
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
@@ -23,15 +24,22 @@ public:
 private slots:
 
 void on_treeWidget_clicked(const QModelIndex &index);
-
+void extraerSubDir(QString,QString);
+void ExtraerFile(QString , QString);
 void on_treeWidget_itemClicked(QTreeWidgetItem *item, int column);
 
 void on_actionopen_triggered();
 
 void on_actionclose_triggered();
 
+void on_actionextract_triggered();
+
+void on_actioninfo_triggered();
+
 private:
+
 Ui::MainWindow *ui;
+Ui::Dialog *ui2;
 };
 
 #endif // MAINWINDOW_H
