d2b5c5f493382ebe6664a92e1e0e40a901a9b1df
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2020-03-20 04:23:56 -0600

    Update Zip.cpp

--- Zip.cpp
+++ Zip.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <sstream>
 #include <QtDebug>
-using namespace std;
 
+using namespace std;
 ZipExplorer::ZipExplorer() {cantidadDirs=0;}
 
 ZipExplorer::~ZipExplorer(){}
@@ -57,19 +57,6 @@ void ZipExplorer::ImprimirLocalHeader(ifstream &Archivo) {
         cantidadDirs++;
         return;
     }
-    /*
-        cout<<"**************LOCAL FILE HEADER**************"<<endl;
-        cout<<"Signature: "<<LocalHeader.signature<<endl;
-        cout<<"versionToExtract: "<<LocalHeader.versionToExtract<<endl;
-        cout<<"generalPurposeBitFlag: "<<LocalHeader.generalPurposeBitFlag<<endl;
-        cout<<"compressionMethod: "<<LocalHeader.compressionMethod<<endl;
-        cout<<"modificationTime: "<<LocalHeader.modificationTime<<endl;
-        cout<<"modificationDate: "<<LocalHeader.modificationDate<<endl;
-        cout<<"crc32: "<<LocalHeader.crc32<<endl;
-        cout<<"compressedSize: "<<LocalHeader.compressedSize<<endl;
-        cout<<"uncompressedSize: "<<LocalHeader.uncompressedSize<<endl;
-        cout<<"filenameLength: "<<LocalHeader.filenameLength<<endl;
-        cout<<"extraFieldLength: "<<LocalHeader.extraFieldLength<<endl;*/
         int espacioR=LocalHeader.filenameLength;
         int espacioR2=LocalHeader.extraFieldLength;
         char *data=new char[espacioR];
@@ -91,15 +78,9 @@ void ZipExplorer::ImprimirLocalHeader(ifstream &Archivo) {
              zipSystem<<Name;
         }
 
-
-        /*
-        cout<<"Filename: "<<LocalInfo.Filename<<endl;
-        cout<<"ExtraField: "<<LocalInfo.extraField<<endl;
-        cout<<"******************************************\n"<<endl;*/
         string Filename(LocalInfo.Filename);
         string ExtraField(LocalInfo.extraField);
         Local+= "********LOCAL FILE HEADER********\n"  "Signature: " +to_string(LocalHeader.signature)+"\n"+ "versionToExtract: "+to_string(LocalHeader.versionToExtract)+"\n"+ "generalPurposeBitFlag: "+to_string(LocalHeader.generalPurposeBitFlag)+"\n"+ "compressionMethod: "+to_string(LocalHeader.compressionMethod)+"\n"+ "modificationTime: "+to_string(LocalHeader.modificationTime)+"\n"+ "modificationDate: "+to_string(LocalHeader.modificationDate)+"\n"+ "crc32: "+to_string(LocalHeader.crc32)+"\n"+ "compressedSize: "+to_string(LocalHeader.compressedSize)+"\n"+ "uncompressedSize: "+to_string(LocalHeader.uncompressedSize)+"\n"+ "filenameLength: "+to_string(LocalHeader.filenameLength)+"\n"+ "extraFieldLength: "+to_string(LocalHeader.extraFieldLength)+"\n"+ "Filename: "+Filename+"\n"+ "ExtraField: "+ExtraField+"************************************\n\n";
-       //cout<<test;
         cantidadDirs++;
 
 }
@@ -125,36 +106,13 @@ void ZipExplorer::getDataDescriptor(ifstream &Archivo) {
 
 void ZipExplorer::ImprimirDataDescriptor(ifstream &Archivo) {
     getDataDescriptor(Archivo);
-    Archivo.read((char*)&dataD,sizeof(DataDescriptor));/*
-    cout<<"**************DATA DESCRIPTOR**************"<<endl;
-    cout<<"crc32: "<<dataD.crc32<<endl;
-    cout<<"compressedSize: "<<dataD.compressedSize<<endl;
-    cout<<"uncompressedSize: "<<dataD.uncompressedSize<<endl;
-    cout<<"******************************************\n"<<endl;*/
+    Archivo.read((char*)&dataD,sizeof(DataDescriptor));
     Local+="**********DATA DESCRIPTOR**********\n" "crc32: "+to_string(dataD.crc32)+"\n" +"compressedSize: "+to_string(dataD.compressedSize)+"\n" +"uncompressedSize: "+to_string(dataD.uncompressedSize)+"\n**************************************\n\n";
 
 }
 
 void ZipExplorer::ImprimirCentralDirectory(ifstream &Archivo) {
-    Archivo.read ((char*)&CentralHeader, sizeof (CentralDirectory));/*
-    cout<<"**************CENTRAL DIRECTORY**************"<<endl;
-    cout<<"signature: "<<CentralHeader.signature<<endl;
-    cout<<"versionMadeby: "<<CentralHeader.versionMadeby<<endl;
-    cout<<"versionToExtract: "<<CentralHeader.versionToExtract<<endl;
-    cout<<"generalPurposeBitFlag: "<<CentralHeader.generalPurposeBitFlag<<endl;
-    cout<<"compressionMethod: "<<CentralHeader.compressionMethod<<endl;
-    cout<<"modificationTime: "<<CentralHeader.modificationTime<<endl;
-    cout<<"modificationDate: "<<CentralHeader.modificationDate<<endl;
-    cout<<"crc32: "<<CentralHeader.crc32<<endl;
-    cout<<"compressedSize: "<<CentralHeader.compressedSize<<endl;
-    cout<<"uncompressedSize: "<<CentralHeader.uncompressedSize<<endl;
-    cout<<"filenameLength: "<<CentralHeader.filenameLength<<endl;
-    cout<<"extraFieldLength: "<<CentralHeader.extraFieldLength<<endl;
-    cout<<"fileComentlength: "<<CentralHeader.fileComentlength<<endl;
-    cout<<"diskNumberStarts: "<<CentralHeader.diskNumberStarts<<endl;
-    cout<<"internalFileAttributes: "<<CentralHeader.internalFileAttributes<<endl;
-    cout<<"externalFileAttributes: "<<CentralHeader.externalFileAttributes<<endl;
-    cout<<"offsetLocalHeader: "<<CentralHeader.offsetLocalHeader<<endl;*/
+    Archivo.read ((char*)&CentralHeader, sizeof (CentralDirectory));
     int espacioR=CentralHeader.filenameLength+CentralHeader.extraFieldLength+CentralHeader.fileComentlength;
     char *data=new char[espacioR];
 
@@ -171,11 +129,6 @@ void ZipExplorer::ImprimirCentralDirectory(ifstream &Archivo) {
     memcpy(CentralInfo.FileComment,data,CentralHeader.fileComentlength);
 
        Central+="**********CENTRAL DIRECTORY**********\n" "signature: "+to_string(CentralHeader.signature)+"\n" +"versionMadeby: "+to_string(CentralHeader.versionMadeby)+"\n" +"generalPurposeBitFlag: "+to_string(CentralHeader.generalPurposeBitFlag)+"\n" +"compressionMethod: "+to_string(CentralHeader.compressionMethod)+"\n" +"modificationTime: "+to_string(CentralHeader.modificationTime)+"\n" +"modificationDate: "+to_string(CentralHeader.modificationDate)+"\n" +"crc32: "+to_string(CentralHeader.crc32)+"\n" +"compressedSize: "+to_string(CentralHeader.compressedSize)+"\n" +"uncompressedSize: "+to_string(CentralHeader.uncompressedSize)+"\n" +"filenameLength: "+to_string(CentralHeader.filenameLength)+ "\n"+"extraFieldLength: "+to_string(CentralHeader.extraFieldLength)+"\n"+"diskNumberStarts: "+to_string(CentralHeader.diskNumberStarts)+"\n"+"internalFileAttributes: "+to_string(CentralHeader.internalFileAttributes)+"\n"+"externalFileAttributes: "+to_string(CentralHeader.externalFileAttributes)+"\n"+"offsetLocalHeader: "+to_string(CentralHeader.offsetLocalHeader)+"\n**************************************\n\n";
-/*
-    cout<<"Filename: "<<CentralInfo.Filename<<endl;
-    cout<<"ExtraField: "<<CentralInfo.extraField<<endl;
-    cout<<"FileComment: "<<CentralInfo.FileComment<<endl;
-    cout<<"*********************************************\n"<<endl;*/
 }
 
 bool ZipExplorer::isDirectory() {
@@ -189,30 +142,14 @@ bool ZipExplorer::isFile() {
 }
 
 void ZipExplorer::ImprimirEndDirectory(ifstream &Archivo) {
-    Archivo.read ((char*)&EndHeader, sizeof (EndDirectory));/*
-    cout<<"**************END DIRECTORY**************"<<endl;
-    cout<<"Signature: "<<EndHeader.signature<<endl;
-    cout<<"numberDisk: "<<EndHeader.numberDisk<<endl;
-    cout<<"DiskWhereStarts: "<<EndHeader.DiskWhereStarts<<endl;
-    cout<<"numberCentralDirectory: "<<EndHeader.numberCentralDirectory<<endl;
-    cout<<"totalNumberDirectory: "<<EndHeader.totalNumberDirectory<<endl;
-    cout<<"sizeofDirectory: "<<EndHeader.sizeofDirectory<<endl;
-    cout<<"offsetDirectory: "<<EndHeader.offsetDirectory<<endl;
-    cout<<"commentLength: "<<EndHeader.commentLength<<endl;*/
-
+    Archivo.read ((char*)&EndHeader, sizeof (EndDirectory));
     int espacioR=EndHeader.commentLength;
     char *data=new char[espacioR];
-
     EndInfo.comment=new char[EndHeader.commentLength];
     Archivo.read(data,EndHeader.commentLength);
     memcpy(EndInfo.comment,data,EndHeader.commentLength);
 
     End+="**********END DIRECTORY**********\n" "Signature: "+to_string(EndHeader.signature)+"\n" +"numberDisk: "+to_string(EndHeader.numberDisk)+"\n" +"DiskWhereStarts: "+to_string(EndHeader.DiskWhereStarts)+"\n" +"numberCentralDirectory: "+to_string(EndHeader.numberCentralDirectory)+"\n" +"totalNumberDirectory: "+to_string(EndHeader.totalNumberDirectory)+"\n" +"sizeofDirectory: "+to_string(EndHeader.sizeofDirectory)+"\n" +"offsetDirectory: "+to_string(EndHeader.offsetDirectory)+"\n" +"commentLength: "+to_string(EndHeader.commentLength)+"\n" +"commentLength: "+to_string(EndHeader.commentLength)+"\n**************************************\n\n";
-
-
-    /*
-    cout<<"comment: "<<  EndInfo.comment<<endl;
-    cout<<"*********************************************"<<endl;*/
 }
 
 
